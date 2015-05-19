98d8bf5bed22bc4c5123bd72861620ef5d2ce6e4
mockenoff <mockenoff@yahoo.com>
2015-05-19 02:43:37 -0500

    Add developer feature

---
+++ js/main.js
@@ -0,0 +1,180 @@
+// Set up variables
+var devbox = document.querySelector('#developer'),
+	canvas = devbox.querySelector('#bubbles'),
+	context = canvas.getContext('2d'),
+
+	// Specifically for particles
+	particles = [],
+	last_draw = undefined,
+
+	// Constants
+	MAX_PARTICLES = 100,
+	CANVAS_WIDTH = 0,
+	CANVAS_HEIGHT = 0;
+
+// Make sure the bubbles show up within the canvas
+function setDimensions(e) {
+	CANVAS_WIDTH = canvas.clientWidth;
+	CANVAS_HEIGHT = canvas.clientHeight;
+	canvas.width = CANVAS_WIDTH;
+	canvas.height = CANVAS_HEIGHT;
+}
+setDimensions();
+window.addEventListener('resize', setDimensions);
+
+// Populate the particles array
+for (var i = 0; i < MAX_PARTICLES; i++) {
+	particles.push(new Particle());
+}
+
+// Particle object
+function Particle() {
+	var self = this,
+		pos_x = 0,
+		pos_y = 0,
+		vel_x = 0,
+		vel_y = 0,
+		acc_x = 0,
+		acc_y = 1.5,
+		color = 0,
+		radius = 0;
+
+	this.reset = function(){
+		// Centralize position
+		self.setPosX(CANVAS_WIDTH / 2);
+		self.setPosY(CANVAS_HEIGHT / 2);
+
+		// Randomize velocities
+		self.setVelX(Math.random() * 40 - 20);
+		self.setVelY(Math.random() * -30);
+
+		// Also randomize the radius
+		self.setRadius(Math.random() * 15);
+
+		// And finally the color
+		self.setColor(Math.random() * 360);
+	};
+
+	this.setColor = function(c){
+		if (typeof(c) === 'number') {
+			color = c;
+		}
+	};
+	this.getColor = function(){
+		return color;
+	};
+
+	this.setRadius = function(r){
+		if (typeof(r) === 'number') {
+			radius = r;
+		}
+	};
+	this.getRadius = function(){
+		return radius;
+	};
+
+	this.setPosX = function(x){
+		if (typeof(x) === 'number') {
+			pos_x = x;
+		}
+	};
+	this.setPosY = function(y){
+		if (typeof(y) === 'number') {
+			pos_y = y;
+		}
+	};
+
+	this.getPosX = function(){
+		return pos_x;
+	};
+	this.getPosY = function(){
+		return pos_y;
+	};
+
+	this.setVelX = function(x){
+		if (typeof(x) === 'number') {
+			vel_x = x;
+		}
+	};
+	this.setVelY = function(y){
+		if (typeof(y) === 'number') {
+			vel_y = y;
+		}
+	};
+
+	this.getVelX = function(){
+		return vel_x;
+	};
+	this.getVelY = function(){
+		return vel_y;
+	};
+
+	this.setAccX = function(x){
+		if (typeof(x) === 'number') {
+			acc_x = x;
+		}
+	};
+	this.setAccY = function(y){
+		if (typeof(y) === 'number') {
+			acc_y = y;
+		}
+	};
+
+	this.getAccX = function(){
+		return acc_x;
+	};
+	this.getAccY = function(){
+		return acc_y;
+	};
+
+	this.step = function(tdiff){
+		// Move position
+		self.setPosX(self.getPosX() + (self.getVelX() * tdiff));
+		self.setPosY(self.getPosY() + (self.getVelY() * tdiff));
+
+		// Alter velocities
+		self.setVelX(self.getVelX() + (self.getAccX() * tdiff));
+		self.setVelY(self.getVelY() + (self.getAccY() * tdiff));
+
+		// Detect boundaries
+		if (self.getPosX() - self.getRadius() <= 0 || self.getPosX() + self.getRadius() >= CANVAS_WIDTH) {
+			self.setVelX(self.getVelX() * -1);
+		}
+		if (self.getPosY() - self.getRadius() >= CANVAS_HEIGHT) {
+			self.reset();
+		}
+	};
+
+	this.reset();
+}
+
+// Drawing function
+function step(timestamp) {
+	if (last_draw === undefined) {
+		last_draw = timestamp;
+	}
+
+	// Clear the canvas
+	context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+	// Draw the particles
+	for (var i = 0; i < MAX_PARTICLES; i++) {
+		context.beginPath();
+		context.fillStyle = 'hsl(' + particles[i].getColor() + ', 80%, 70%)';
+		context.arc(particles[i].getPosX(), particles[i].getPosY(), particles[i].getRadius(), Math.PI*2, false);
+		context.fill();
+
+		// Force particle movement calculations
+		particles[i].step((timestamp - last_draw) / 50);
+	}
+
+	last_draw = timestamp;
+	window.requestAnimationFrame(step);
+}
+window.requestAnimationFrame(step);
+
+document.addEventListener('keydown', function(e){
+	if (e.which === 32) {
+		window.requestAnimationFrame(step);
+	}
+});
